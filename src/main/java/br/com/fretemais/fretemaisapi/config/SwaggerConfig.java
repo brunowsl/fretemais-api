7b2ef1dbb7c49630d8bcd0a9e057dcb6b25891af
Bruno Lobão <brunowsl123@gmail.com>
2021-08-19 20:02:39 -0300

    final fix for deploy on heroku

--- src/main/java/br/com/fretemais/fretemaisapi/config/SwaggerConfig.java
+++ src/main/java/br/com/fretemais/fretemaisapi/config/SwaggerConfig.java
@@ -17,8 +17,8 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("br.com.fretemais.fretemaisapi.controller"))
+				.paths(PathSelectors.ant("/**"))
 				.build();
 	}
 
