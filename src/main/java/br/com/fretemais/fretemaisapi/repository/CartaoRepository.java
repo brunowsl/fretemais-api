b1f2012361b946fc7d125a4d4c149e2ab13c6af1
Bruno Lobão <brunowsl123@gmail.com>
2021-09-28 20:23:38 -0300

    ok

--- src/main/java/br/com/fretemais/fretemaisapi/repository/CartaoRepository.java
+++ src/main/java/br/com/fretemais/fretemaisapi/repository/CartaoRepository.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.fretemais.fretemaisapi.model.Cartao;
 
 public interface CartaoRepository extends JpaRepository<Cartao, Integer> {
 
-	@Query(value = "SELECT * FROM CARTAO c WHERE c.usuario_id = ?1", nativeQuery = true)
-	public List<Cartao> findByUsuarioUsuarioId(Integer usuarioId);
+	@Query(value = "SELECT * FROM fretemais.CARTAO c WHERE c.usuario_id = :usuarioId", nativeQuery = true)
+	public List<Cartao> findByUsuarioUsuarioId(@Param("usuarioId") Integer usuarioId);
 
 }
